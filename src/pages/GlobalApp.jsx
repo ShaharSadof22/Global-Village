e149b214c73efdfe4a36e979caed39d1d5cd52b1
Shlomikop <64078705+Shlomikop@users.noreply.github.com>
2020-10-28 16:07:08 +0200

    ש

--- src/pages/GlobalApp.jsx
+++ src/pages/GlobalApp.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
-
-import {Globe} from '../cmps/Globe'
-import {FilterCountry} from '../cmps/FilterCountry'
+import { Globe } from '../cmps/Globe'
+import { FilterCountry } from '../cmps/FilterCountry'
+import { SideBar } from '../cmps/SideBar'
 
 export function GlobalApp() {
     return (
         <div>
             <p>GlobalApp</p>
+            <SideBar />
             <Globe />
             <FilterCountry />
         </div>
