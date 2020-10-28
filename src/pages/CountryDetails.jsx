77d07b51713c0c348ce0741aeca7cc06fbf8fd56
Shlomikop <64078705+Shlomikop@users.noreply.github.com>
2020-10-28 15:01:38 +0200

    123

--- src/pages/CountryDetails.jsx
+++ src/pages/CountryDetails.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import {SideBar} from '../cmps/SideBar.jsx'
 
 export function CountryDetails() {
     return (
         <div>
             <p>CountryDetails</p>
+            <SideBar />
         </div>
     )
 }
