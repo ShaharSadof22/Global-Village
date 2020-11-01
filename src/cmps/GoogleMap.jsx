d2ac3ac0706436d92c23c4de3cd53bc00f74a301
arikeinav <68658683+arikeinav@users.noreply.github.com>
2020-11-01 12:43:45 +0200

    seg

--- src/cmps/GoogleMap.jsx
+++ src/cmps/GoogleMap.jsx
@@ -1,10 +1,46 @@
-import React from 'react'
+import React from "react";
+import { GoogleApiWrapper, InfoWindow, Marker, Map } from 'google-maps-react';
+class _GoogleMap extends React.Component {
+  state = {
+    lat: 32.11387923078556,
+    lng: 34.82767207691768,
+  }
 
-export function GoogleMap() {
+  render() {
+    const containerStyle = {
+        position: 'absolute',  
+        width: '350px',
+        height: '350px',
+        right:'100px'
+
+      }
     return (
-        <div>
-        <p>GoogleMap</p>
-            
-        </div>
-    )
+     <Map
+        initialCenter={this.state}
+        center={this.state}
+        google={this.props.google}
+        zoom={12}
+        containerStyle={containerStyle}
+       
+        
+      >
+        <Marker
+          name={"France"}
+          position={{ lat: 32.0657353594228, lng: 34.87023370314972 }}
+        />
+       
+
+        <InfoWindow position={this.state} visible={true}>
+          <div></div>
+        </InfoWindow>
+
+        
+        
+      </Map> 
+    );
+  }
 }
+
+export const GoogleMap = GoogleApiWrapper({
+  apiKey: "AIzaSyApO-6umWfoouV0eoggSlSSD5NNrYJqUrE"
+})(_GoogleMap);
--- src/pages/CountryDetails.jsx
+++ src/pages/CountryDetails.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import {SideBar} from '../cmps/SideBar.jsx'
+import {GoogleMap} from '../cmps/GoogleMap'
 
 export function CountryDetails() {
     return (
         <div>
             <p>CountryDetails</p>
             <SideBar />
+          <GoogleMap/>
         </div>
     )
 }
